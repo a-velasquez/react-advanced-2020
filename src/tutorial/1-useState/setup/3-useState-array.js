d437c63330df9e05dd1113a09e426385c7edf71b
Alex Velazquez <avelasquez075@gmail.com>
2021-04-30 01:26:36 -0700

    add button to clear and delete items and removeItem event handler

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,7 +3,14 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   // useState can also be imported with React, { useState }
-  const [people, setPeople] = React.useState(data); 
+  const [people, setPeople] = React.useState(data);
+  
+  const removeItem = (id) => {
+    // returns new array
+    let newPeople = people.filter((person) => person.id !== id)
+    // passing setPeople new array
+    setPeople(newPeople)
+  }
   return (
     // <></> same as React.Fragment
     <>
@@ -15,11 +22,16 @@ const UseStateArray = () => {
           return (
             <div key={id} className='item'>
               <h4>{name}</h4>
+              {/* calling arrow function so eventHandler doesn't automatically run on render of page */}
+              <button onClick={() => removeItem(id)}>Remove Item</button>
             </div>
           );
-        })};
+        })}
+      {/* calling function inline versus creating handler */}
+      <button className='btn' onClick={() => setPeople([])}>Clear Items</button>
     </>
   );
 };
 
 export default UseStateArray;
+ 
