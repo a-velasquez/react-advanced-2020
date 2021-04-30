2daffd1758e762c6c918724b06f4813b22613ed3
Alex Velazquez <avelasquez075@gmail.com>
2021-04-30 00:36:41 -0700

    add handleClick to change title, add fragment with button

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
+// useState is a React function that takes a value (string, number, boolean, array)
+// invoking useState returns an array with 2 values, the value and the function that controls that value
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState(['hello world'));
+
+  // destructured way
+  //    value, function
+  const [text, setText] = useState("random title");
+
+  // setText calls useState and passes argument to change title
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText("hello world");
+    } else {
+      setText('random title')
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
