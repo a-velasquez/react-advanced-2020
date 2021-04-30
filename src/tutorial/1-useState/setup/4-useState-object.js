daa2c075828096b7d673b8c7b26d1c1a80687072
Alex Velazquez <avelasquez075@gmail.com>
2021-04-30 13:44:43 -0700

    add changeMessage handler, return person, use destrucutred person obj

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({ name: 'peter', age: 24, message: 'random message' })
+
+
+  /* using destructured handlers to setState
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message') */
+
+  const changeMessage = () => {
+    // setMessage('hello world')
+    if (person.message === 'random message') {
+      setPerson({ ...person, message: 'hello world' });
+    } else {
+      setPerson({ ...person, message: 'random message'})
+    }
+  }
+  
+  return (
+    <>
+      {/* if using destructured, you would just call name, age and message without person*/}
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
 };
 
 export default UseStateObject;
