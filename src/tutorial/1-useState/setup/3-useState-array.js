49b7bf6e1049826e8745759ef5cc3214ac578d59
Alex Velazquez <avelasquez075@gmail.com>
2021-04-30 01:06:16 -0700

    map over array of data and use useState hook

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,24 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // useState can also be imported with React, { useState }
+  const [people, setPeople] = React.useState(data); 
+  return (
+    // <></> same as React.Fragment
+    <>
+      {
+        people.map((person) => {
+          // destructured method of accessing values of person object
+          const { id, name } = person;
+          console.log(person)
+          return (
+            <div key={id} className='item'>
+              <h4>{name}</h4>
+            </div>
+          );
+        })};
+    </>
+  );
 };
 
 export default UseStateArray;
