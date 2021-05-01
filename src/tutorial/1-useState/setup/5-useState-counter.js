112369d57fae97dd20e65ffaa48c68c62565d9fc
Alex Velazquez <avelasquez075@gmail.com>
2021-05-01 16:30:28 -0700

    add complexIncrease with delayed increase, section with button

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,6 +3,17 @@ import React, { useState } from 'react';
 const UseStateCounter = () => {
 
   const [value, setValue] = useState(0);
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevState) => {
+        console.log(prevState)
+        return prevState + 1;
+      });
+    }, 2000);
+  };
+
   const reset = () => {
     setValue(0)
   }
@@ -16,10 +27,17 @@ const UseStateCounter = () => {
           decrease
         </button>
         <button className="btn" onClick={reset}>
-          resest
+          reset
         </button>
         <button className="btn" onClick={() => setValue(value + 1)}>
           increase
+        </button>
+      </section>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          increase later...
         </button>
       </section>
     </>
