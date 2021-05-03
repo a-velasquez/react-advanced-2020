4a513db4ebbfd59b3f46bec0d72fcca9ca956177
Alex Velazquez <avelasquez075@gmail.com>
2021-05-02 18:08:13 -0700

    add ternary operators to conditionaly render paragraph and error

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,18 +4,31 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('Peter');
+  const [isError, setIsError] = useState(false)
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
 
   return (
     <>
-      <h1>{text || 'john doe'}</h1>
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+
       {/* if text is truthy render 'hello world' */}
-      { text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there was an error.</p>
+      ) : (
+        <div>
+          <h2>There is no error.</h2>
+        </div>
+      )}
+
       {/* if text falsey render 'name is missing' */}
-      { !text && <h1>name is missing</h1>}
+      {/* { !text && <h1>name is missing</h1>} */}
     </>
-  )
+  );
 };
 
 export default ShortCircuit;
