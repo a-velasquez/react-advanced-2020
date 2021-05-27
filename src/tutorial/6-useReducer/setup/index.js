05360d4fc9f1e6e828dc8e95b7150aa7468b1d4f
Alex Velazquez <avelasquez075@gmail.com>
2021-05-27 13:15:09 -0700

    imprt reducer, add dfltState, local state and useReducer, onChng to input

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,14 +1,23 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
+import { reducer } from '../final/reducer';
 // reducer function
 
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: ''
+}
+
 const Index = () => {
+  const [name, setName] = useState('')
+  const[state, dispatch] = useReducer(reducer, defaultState)
   return (
     <>
       <form className='form'>
         <div>
-          <input type='text' value='' ></input>
+          <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
         </div>
         <button type='submit'>add</button>
       </form>
