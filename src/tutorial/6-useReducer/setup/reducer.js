365f4d855c735e6e6b5dba53d3b415533a1fc3b3
Alex Velazquez <avelasquez075@gmail.com>
2021-05-27 15:02:58 -0700

    add NO-VALUE action if no value given, throw error

--- src/tutorial/6-useReducer/setup/reducer.js
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -1,12 +1,15 @@
 export const reducer = (state, action) => {
-  if (action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload]
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: 'true',
-      modalContent: 'item added'
-    }
-  }
-  return {...state}
+	if (action.type === 'ADD_ITEM') {
+		const newPeople = [...state.people, action.payload]
+		return {
+			...state,
+			people: newPeople,
+			isModalOpen: 'true',
+			modalContent: 'item added'
+		}
+	}
+	if (action.type === 'NO_VALUE') {
+		return {...state, isModalOpen: true, modalContent: 'please enter a value'}
+	}
+	throw new Error('no matching action type')
 }
