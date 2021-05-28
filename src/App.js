126b148d20f4ee3249e02f343af0b03bfedac598
Alex Velazquez <avelasquez075@gmail.com>
2021-05-28 13:43:58 -0700

    import and render './tutorial/8-useContext/setup/1-context-api' component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Setup from './tutorial/6-useReducer/setup'
+import React from 'react'
+import Setup from './tutorial/8-useContext/setup/1-context-api'
 function App() {
-  return (
-    <div className="container">
-      <Setup />
-    </div>
-  );
+	return (
+		<div className='container'>
+			<Setup />
+		</div>
+	)
 }
 
-export default App 
+export default App
