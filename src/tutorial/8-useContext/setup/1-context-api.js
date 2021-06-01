f9c3523906e7dd47ba87800d153a1158fbc43323
Alex Velazquez <avelasquez075@gmail.com>
2021-06-01 11:17:30 -0700

    add useContext Provider, pass removePerson to List directly

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,9 @@ import {data} from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext()
+// returns Provider, Consumer components
+
 const ContextAPI = () => {
 	const [people, setPeople] = useState(data)
 	const removePerson = (id) => {
@@ -11,30 +14,25 @@ const ContextAPI = () => {
 		})
 	}
 	return (
-		<>
-			<h3>prop drilling</h3>
-			<List people={people} removePerson={removePerson} />
-		</>
+		<PersonContext.Provider value={{removePerson}}>
+			<h3>Context API/useContext</h3>
+			<List />
+		</PersonContext.Provider>
 	)
 }
 
-const List = ({people, removePerson}) => {
+const List = ({people}) => {
 	return (
 		<>
 			{people.map((person) => {
-				return (
-					<SinglePerson
-						key={person.id}
-						{...person}
-						removePerson={removePerson}
-					/>
-				)
+				return <SinglePerson key={person.id} {...person} />
 			})}
 		</>
 	)
 }
 
-const SinglePerson = ({id, name, removePerson}) => {
+const SinglePerson = ({id, name}) => {
+	const {removePerson} = useContext(PersonContext)
 	return (
 		<div className='item'>
 			<h4>{name}</h4>
