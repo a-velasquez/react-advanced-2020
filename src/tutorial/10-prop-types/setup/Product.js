547a6f384e9ad422e679e193453455dd2eecfbf2
Alex Velazquez <avelasquez075@gmail.com>
2021-06-03 13:26:25 -0700

    use circuit operators to use default image and price

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({image, name, price}) => {
+	const url = image && image.url
 	return (
 		<article className='product'>
-			{/* <img src={image.url} alt={name} />
+			<img src={url || defaultImage} alt={name} />
 			<h4>{name}</h4>
-			<p>${price}</p> */}
+			<p>${price || 3.99}</p>
 		</article>
 	)
 }
