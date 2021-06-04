4e4e367a0640a180b9e870d80e56596bfd1aa8a6
Alex Velazquez <avelasquez075@gmail.com>
2021-06-04 12:59:11 -0700

    add notes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({image, name, price}) => {
+	// checks if image is truthy, if so checks for image.url
 	const url = image && image.url
 	return (
 		<article className='product'>
+			{/* if object image has url else render defaultImage */}
 			<img src={url || defaultImage} alt={name} />
 			<h4>{name}</h4>
 			<p>${price || 3.99}</p>
