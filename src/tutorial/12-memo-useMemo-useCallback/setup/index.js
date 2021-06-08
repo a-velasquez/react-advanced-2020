c2acdecf06d3d800eb273096bf97ab8a30dd2ea9
Alex Velazquez <avelasquez075@gmail.com>
2021-06-07 18:57:46 -0700

    add calculateMostExpensive(), use useMemo() to call when products changes

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -5,6 +5,18 @@ import {useFetch} from '../../9-custom-hooks/final/2-useFetch'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
+const calculateMostExpensive = (data) => {
+	return (
+		data.reduce((total, item) => {
+			const price = item.fields.price
+			if (price >= total) {
+				total = price
+			}
+			return total
+		}, 0) / 100
+	)
+}
+
 // every time props or state changes, component re-renders
 
 const Index = () => {
@@ -16,6 +28,10 @@ const Index = () => {
 		setCart(cart + 1)
 	}, [cart])
 
+	const mostExpensive = useMemo(() => {
+		calculateMostExpensive(products)
+	}, [products])
+
 	return (
 		<>
 			<h1>Count : {count}</h1>
@@ -23,6 +39,7 @@ const Index = () => {
 				click me
 			</button>
 			<h1 style={{marginTop: '3rem'}}>cart: {cart}</h1>
+			<h1>Most expensive : ${calculateMostExpensive(products)}</h1>
 			<BigList products={products} addToCart={addToCart} />
 		</>
 	)
